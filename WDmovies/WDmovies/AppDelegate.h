fbabf7b347f8d174a82a4354d9c3db8c3cec0747
何晨旭 <hechenxu@hechenxudeMacBook-Air.local>
2019-06-10 10:53:00 +0800

    ’添加项目‘

---
+++ WDmovies/WDmovies/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WDmovies
+//
+//  Created by 何晨旭 on 2019/6/10.
+//  Copyright © 2019年 何晨旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WDmovies/WDmovies/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WDmovies
+//
+//  Created by 何晨旭 on 2019/6/10.
+//  Copyright © 2019年 何晨旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
